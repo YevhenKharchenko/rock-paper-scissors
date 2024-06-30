fcd1b95e103d6918d38b74736e47e699000c1ffb
YevhenKharchenko <evgenvalerych@gmail.com>
2024-06-30 11:18:54 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+import aiPaper from '../img/ai-paper.png';
+import aiRock from '../img/ai-rock.png';
+import aiScissors from '../img/ai-scissors.png';
+import aiArm from '../img/AI-Arm-Idle.png';
+import humanPaper from '../img/human-paper.png';
+import humanRock from '../img/human-rock.png';
+import humanScissors from '../img/human-scissors.png';
+import humanArm from '../img/Human-Arm-Idle.png';
+
 const rockBtn = document.querySelector('.rock');
 const paperBtn = document.querySelector('.paper');
 const scissorsBtn = document.querySelector('.scissors');
@@ -10,11 +19,11 @@ const resultText = document.querySelector('.result-text');
 const nextRoundBtn = document.querySelector('.next-round-btn');
 const restartBtn = document.querySelector('.restart-btn-img');
 const restartBtnWrapper = document.querySelector('.restart-btn-wrapper');
-
 const scoreAi = document.querySelector('.score-ai');
 const scoreHuman = document.querySelector('.score-human');
 const optionChoiceWrapper = document.querySelector('.choice-wrapper');
 const options = ['rock', 'paper', 'scissors'];
+
 let countAi = 0;
 let countHuman = 0;
 
@@ -28,62 +37,62 @@ function onOptionBtnClick(e) {
   aiArmImg.style.transform = 'rotate(50deg)';
 
   if (e.target === rockImg && aiChoice === 'rock') {
-    humanArmImg.src = './img/human-rock.png';
-    aiArmImg.src = './img/ai-rock.png';
+    humanArmImg.src = humanRock;
+    aiArmImg.src = aiRock;
     resultText.textContent = "IT'S A DRAW";
   }
 
   if (e.target === rockImg && aiChoice === 'paper') {
-    humanArmImg.src = './img/human-rock.png';
-    aiArmImg.src = './img/ai-paper.png';
+    humanArmImg.src = humanRock;
+    aiArmImg.src = aiPaper;
     countAi++;
     resultText.textContent = 'YOU LOOSE!';
   }
 
   if (e.target === rockImg && aiChoice === 'scissors') {
-    humanArmImg.src = './img/human-rock.png';
-    aiArmImg.src = './img/ai-scissors.png';
+    humanArmImg.src = humanRock;
+    aiArmImg.src = aiScissors;
     countHuman++;
     resultText.textContent = 'YOU WIN!';
   }
 
   if (e.target === paperImg && aiChoice === 'rock') {
-    humanArmImg.src = './img/human-paper.png';
-    aiArmImg.src = './img/ai-rock.png';
+    humanArmImg.src = humanPaper;
+    aiArmImg.src = aiRock;
     countHuman++;
     resultText.textContent = 'YOU WIN!';
   }
 
   if (e.target === paperImg && aiChoice === 'paper') {
-    humanArmImg.src = './img/human-paper.png';
-    aiArmImg.src = './img/ai-paper.png';
+    humanArmImg.src = humanPaper;
+    aiArmImg.src = aiPaper;
     resultText.textContent = "IT'S A DRAW";
   }
 
   if (e.target === paperImg && aiChoice === 'scissors') {
-    humanArmImg.src = './img/human-paper.png';
-    aiArmImg.src = './img/ai-scissors.png';
+    humanArmImg.src = humanPaper;
+    aiArmImg.src = aiScissors;
     countAi++;
     resultText.textContent = 'YOU LOOSE!';
   }
 
   if (e.target === scissorsImg && aiChoice === 'rock') {
-    humanArmImg.src = './img/human-scissors.png';
-    aiArmImg.src = './img/ai-rock.png';
+    humanArmImg.src = humanScissors;
+    aiArmImg.src = aiRock;
     countAi++;
     resultText.textContent = 'YOU LOOSE!';
   }
 
   if (e.target === scissorsImg && aiChoice === 'paper') {
-    humanArmImg.src = './img/human-scissors.png';
-    aiArmImg.src = './img/ai-paper.png';
+    humanArmImg.src = humanScissors;
+    aiArmImg.src = aiPaper;
     countHuman++;
     resultText.textContent = 'YOU WIN!';
   }
 
   if (e.target === scissorsImg && aiChoice === 'scissors') {
-    humanArmImg.src = './img/human-scissors.png';
-    aiArmImg.src = './img/ai-scissors.png';
+    humanArmImg.src = humanScissors;
+    aiArmImg.src = aiScissors;
     resultText.textContent = "IT'S A DRAW";
   }
 
@@ -101,8 +110,8 @@ function onRestartBtnClick(e) {
   scoreAi.textContent = 0;
   scoreHuman.textContent = 0;
   resultText.textContent = '';
-  humanArmImg.src = './img/Human-Arm-Idle.png';
-  aiArmImg.src = './img/AI-Arm-Idle.png';
+  humanArmImg.src = humanArm;
+  aiArmImg.src = aiArm;
   restartBtnWrapper.style.display = 'none';
   optionChoiceWrapper.style.display = 'flex';
   humanArmImg.style.transform = 'rotate(0deg)';
@@ -114,8 +123,8 @@ nextRoundBtn.addEventListener('click', onNextRoundBtnClick);
 function onNextRoundBtnClick(e) {
   restartBtnWrapper.style.display = 'none';
   optionChoiceWrapper.style.display = 'flex';
-  humanArmImg.src = './img/Human-Arm-Idle.png';
-  aiArmImg.src = './img/AI-Arm-Idle.png';
+  humanArmImg.src = humanArm;
+  aiArmImg.src = aiArm;
   resultText.textContent = '';
   humanArmImg.style.transform = 'rotate(0deg)';
   aiArmImg.style.transform = 'rotate(0deg)';
